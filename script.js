72be45b74fc2ea0264c0c8fd2b393cf02d1579df
Lauliek <laureline.pellegrini@gmail.com>
2024-10-29 11:53:09 +0100

    affichage de la bonne réponse en cas d'erreur

--- script.js
+++ script.js
@@ -68,7 +68,9 @@ async function fetchAndDisplayJSON() {
                     elementClique.style.backgroundColor = "green"; // Bonne réponse
                 } else {
                     elementClique.style.backgroundColor = "red"; // Mauvaise réponse
-                    indexDelaBonneReponse.style.backgroundColor = "green"; // on peut selectionner tant que la bonne réponse n 'est pas trouvée, en suite clic impossible
+                    //indexDelaBonneReponse.style.backgroundColor = "green"; // on peut selectionner tant que la bonne réponse n 'est pas trouvée, en suite clic impossible
+                    list.children[indexDelaBonneReponse].style.backgroundColor = "green"; // affichage de la bonne réponse en cas d'erreur
+
                 }
             // Désactivation des clics sur toutes les réponses après la sélection d'une réponse
                 Array.from(list.children).forEach((li) => {
